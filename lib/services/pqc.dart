c315b2408420e95689ee300461ee3ea5ccbe3fc2
bardia <bardiakzm@gmail.com>
2025-07-19 09:03:55 -0700

    removed_liboqs_local_files

--- lib/services/pqc.dart
+++ lib/services/pqc.dart
@@ -1,14 +1,1 @@
-import 'dart:ffi';
-import 'package:qrypt/oqs_bindings.dart'; // this contains OQS_KEM_kyber_512_new
-
-void main() {
-  // Load the DLL
-  final dylib = DynamicLibrary.open('liboqs/oqs.dll');
-
-  final kem = OQS_KEM_kyber_512_new(); // returns Pointer<OQS_KEM>
-
-  print('Public key length: ${kem.ref.length_public_key}');
-
-  // Clean up
-  OQS_KEM_free(kem);
-}
+void main() {}
