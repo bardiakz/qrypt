21081d01d7e99ecfea3a551759ae7727f62bb416
bardia <bardiakzm@gmail.com>
2025-07-26 06:54:42 -0700

    saveTheme

--- lib/providers/resource_providers.dart
+++ lib/providers/resource_providers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 enum AppMode { encrypt, decrypt }
 
@@ -17,14 +18,65 @@ final currentTextControllerProvider = StateProvider<TextEditingController>(
 final isDarkModeProvider = StateProvider<bool>((ref) => false);
 
 class ThemeModeNotifier extends StateNotifier<ThemeMode> {
-  ThemeModeNotifier() : super(ThemeMode.light);
+  static const String _themeKey = 'app_theme_mode';
+  static const _storage = FlutterSecureStorage();
+
+  ThemeModeNotifier() : super(ThemeMode.system) {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    try {
+      final savedTheme = await _storage.read(key: _themeKey);
+      if (savedTheme != null) {
+        switch (savedTheme) {
+          case 'light':
+            state = ThemeMode.light;
+            break;
+          case 'dark':
+            state = ThemeMode.dark;
+            break;
+          case 'system':
+            state = ThemeMode.system;
+            break;
+          default:
+            state = ThemeMode.system;
+        }
+      }
+    } catch (e) {
+      state = ThemeMode.light;
+    }
+  }
+
+  Future<void> _saveTheme(ThemeMode mode) async {
+    try {
+      String themeString;
+      switch (mode) {
+        case ThemeMode.light:
+          themeString = 'light';
+          break;
+        case ThemeMode.dark:
+          themeString = 'dark';
+          break;
+        case ThemeMode.system:
+          themeString = 'system';
+          break;
+      }
+      await _storage.write(key: _themeKey, value: themeString);
+    } catch (e) {
+      debugPrint('Error saving theme: $e');
+    }
+  }
 
   void toggleTheme() {
-    state = state == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    final newMode = state == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    state = newMode;
+    _saveTheme(newMode);
   }
 
   void setTheme(ThemeMode mode) {
     state = mode;
+    _saveTheme(mode);
   }
 }
 
