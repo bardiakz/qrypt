81884a7c0e402096dc947ea85e285164013fcd29
bardia <bardiakzm@gmail.com>
2025-07-26 09:38:39 -0700

    lazyCommit

--- lib/services/input_handler.dart
+++ lib/services/input_handler.dart
@@ -834,15 +834,3 @@ class InputHandler {
     );
   }
 }
-
-// class DeQrypt{
-//   String text="";
-//   late final EncryptionMethod encryption;
-//   late final ObfuscationMethod obfuscation;
-//   bool useTag=false;
-//   String? tag;
-//   DeQrypt.withTag({required this.text,required this.encryption,required this.obfuscation,this.tag}){
-//     useTag=true;
-//   }
-//   DeQrypt({required this.text,required this.encryption,required this.obfuscation});
-// }
